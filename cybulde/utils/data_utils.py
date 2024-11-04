aa792c4e1be2a909ae0f7b904722a854bec237a6
Salih SARITEPE <salih-srt45@hotmail.com>
2024-11-04 12:51:22 +0100

    Updated version of data from v2 to v3

--- cybulde/utils/data_utils.py
+++ cybulde/utils/data_utils.py
@@ -15,14 +15,11 @@ def initialize_dvc() -> None:
         DATA_UTILS_LOGGER.info("DVC is already initialized")
         return
     DATA_UTILS_LOGGER.info("Initializing DVC")
-    run_shell_command("dvc init")
-    print("init!")
+    run_shell_command("dvc init")    
     run_shell_command("dvc config core.analytics false")
     run_shell_command("dvc config core.autostage true")
-    run_shell_command("git add .dvc")
-    print("add !")
-    run_shell_command("git commit -nm 'Initiliazed DVC'")
-    print("committed")
+    run_shell_command("git add .dvc")    
+    run_shell_command("git commit -nm 'Initiliazed DVC'")    
 
 
 def initialize_dvc_storage(dvc_remote_name: str, dvc_remote_url: str) -> None:
